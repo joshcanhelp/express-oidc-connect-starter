b0e3bfc8942a77da05c8eac2185dd7b3929b9679
Josh Cunningham <josh@joshcanhelp.com>
2019-09-03 15:04:16 -0700

    Adding authentication

--- app.js
+++ app.js
@@ -4,17 +4,34 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 
+const bodyParser = require('body-parser');
+const session = require('cookie-session');
+const { auth, requiresAuth } = require('express-openid-connect');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(morgan('combined'));
 
+app.use(bodyParser.urlencoded({ 
+  extended: false 
+}));
+
+app.use(session({
+  name: process.env.SESSION_NAME,
+  secret: process.env.COOKIE_SECRET
+}));
+
+app.use(auth({
+  required: false
+}));
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/expenses', (req, res) => {
+app.get('/expenses', requiresAuth(), (req, res) => {
   res.render('expenses', {
     expenses: [
       {
